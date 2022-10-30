83ece9dc8a43360831f2ed7ad2fcaf0988746df5
Praisino <praisetaiwo@gmail.com>
2022-10-30 22:36:00 +0100

    changed the lambda function

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://tuc7u24uc2.execute-api.eu-west-1.amazonaws.com/default/cc_Predict",
+                url: "https://h8awrzww33.execute-api.eu-west-1.amazonaws.com/default/cc_function2",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
